677efe48a8010637760b1b39548fa81f4acbb4b7
p901ett <p901ett@mosc02dp6xamd6r.lan>
2021-01-19 20:04:20 +0200

    Initial commit

---
+++ src/test/java/com/enorkus/cqrses/CqrsesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enorkus.cqrses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CqrsesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
